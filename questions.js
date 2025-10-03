0318cd148d2026e5e02caa48deb5a35b7046a033
Ancclara <santossilvaanaclara995@gmail.com>
2025-10-02 21:20:21 -0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,11 @@
+export default[
+    {
+        question: "Qual é a função utilizada para exibir uma mensagem em uma janela simples de alerta no navegador?",
+        answers: [
+            { ooption: "window.prompt()", correct: false},
+            {option: "window.alert()", correct: true},
+            {option: "console.log()", correct: false},
+            {option: "window.confirm()", correct: false},
+        ],
+    },
+]
